54ee326db13fae5724496b03215cd30525a3c512
Joshua <joshua.thomas2907@gmail.com>
2022-04-02 15:51:29 +1100

    group register and login under auth, removes unneeded elements

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,7 @@ import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Surveys from "../views/Surveys.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import store from "../store";
 
 const routes = [
@@ -26,14 +27,23 @@ const routes = [
         ],
     },
     {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: Register,
+        path: "/auth",
+        redirect: "/login",
+        name: "Auth",
+        meta: { isGuest: true },
+        component: AuthLayout,
+        children: [
+            {
+                path: "/login",
+                name: "Login",
+                component: Login,
+            },
+            {
+                path: "/register",
+                name: "Register",
+                component: Register,
+            },
+        ],
     },
 ];
 
@@ -48,10 +58,7 @@ router.beforeEach((to, from, next) => {
     // redirect to Dashboard, otherwise continue
     if (to.meta.requiresAuth && !store.state.user.token) {
         next({ name: "Login" });
-    } else if (
-        store.state.user.token &&
-        (to.name === "Login" || to.name === "Register")
-    ) {
+    } else if (store.state.user.token && to.meta.isGuest) {
         next({ name: "Dashboard" });
     } else {
         next();
