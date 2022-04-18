6f8dd53b5e27ba78f6634539792ce002b919ea32
Joshua <joshua.thomas2907@gmail.com>
2022-04-18 12:29:40 +1000

    adds survery question/answer tables, adds register route

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,22 +1,9 @@
 <template>
-    <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
-        </div>
-    </header>
-    <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <div class="px-4 py-6 sm:px-0">Content Here</div>
-        </div>
-    </main>
+    <PageComponent title="Dashboard"> Content goes here </PageComponent>
 </template>
 
-<script>
-export default {
-    setup() {
-        return {};
-    },
-};
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
 </script>
 
 <style scoped lang="scss"></style>
